2965b6497c9df8c6ddfc0c5871f088cb6c12d95c
haihua sun <2660245132@qq.com>
2024-04-27 03:33:42 +0800

    basic Template

---
+++ app/src/main/java/com/example/bluetooth_test/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.bluetooth_test
+
+
+import android.content.Intent
+import android.os.Build
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import androidx.appcompat.app.AppCompatActivity
+
+
+// Permission code from:
+// https://stackoverflow.com/questions/67722950/android-12-new-bluetooth-permissions
+
+class MainActivity : AppCompatActivity() {
+
+    private val _tag = MainActivity::class.qualifiedName
+
+    @RequiresApi(Build.VERSION_CODES.TIRAMISU)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Log.d(_tag, "Starting...")
+        if (getSupportActionBar() != null){
+            getSupportActionBar()?.hide();
+        }
+        setContentView(R.layout.activity_main)
+
+//        var myBluetooth = MyBluetooth(this)
+//
+//        val sendAButton: Button = findViewById(R.id.btnSendA)
+//        sendAButton.setOnClickListener { myBluetooth.writeData("a") }
+//
+//        val sendBButton: Button = findViewById(R.id.btnSendB)
+//        sendBButton.setOnClickListener { myBluetooth.writeData("b") }
+//
+//        val receiveButton: Button = findViewById(R.id.btnReceive)
+//        receiveButton.setOnClickListener {
+//            val s = myBluetooth.readData()
+//            Toast.makeText(applicationContext, "Received text: $s", Toast.LENGTH_LONG).show()
+//        }
+//
+        val connectButton: Button = findViewById(R.id.btnConnect)
+        connectButton.setOnClickListener {
+            intent = Intent(this@MainActivity, BluetoothActivity::class.java)
+            startActivity(intent)
+        }
+
+
+    }
+}
